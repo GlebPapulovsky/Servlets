0091f7181877af6353ba14db0c721ba6edb339ca
GlebPapulovsky <papulovskijglech@gmail.com>
2025-02-26 21:18:29 +0300

    formated

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -2,8 +2,6 @@ package ru.netology.servlet;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +16,7 @@ public class MainServlet extends HttpServlet {
   @Override
   public void init() {
     final var context= new AnnotationConfigApplicationContext("ru.netology");
-    
+
     controller = context.getBean(PostController.class);
 
   }
